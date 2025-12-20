e19ca05a945f4d0c433afae63594ff157112cdc3
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-12-20 05:16:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2684997814-10180",
+  "appId": "1:269811218512:web:dc328512e0d6d493ec3955",
+  "apiKey": "AIzaSyBlnEicgwysoJ9p3St9HzxwAMZpQOpkulQ",
+  "authDomain": "studio-2684997814-10180.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "269811218512"
+};
