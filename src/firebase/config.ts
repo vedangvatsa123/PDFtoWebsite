dcbe51af62c63c51d6ee55c6f0df75a090fe963d
Vedang R. Vatsa <vatsavedang@gmail.com>
2025-12-20 08:53:30 +0000

    ensure there are no publicly accessible api keys

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-2684997814-10180",
-  "appId": "1:269811218512:web:dc328512e0d6d493ec3955",
-  "apiKey": "AIzaSyBlnEicgwysoJ9p3St9HzxwAMZpQOpkulQ",
-  "authDomain": "studio-2684997814-10180.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "269811218512"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
 };
