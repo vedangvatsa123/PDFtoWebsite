ff1d655a5bef4b0335ac2229e68847c15b278c19
Vedang R. Vatsa <vatsavedang@gmail.com>
2026-01-09 13:10:51 +0000

    make the header transparent

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -35,7 +35,7 @@ export default function Header({ children }: { children?: React.ReactNode }) {
     }
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full">
       <div className="container flex h-16 max-w-screen-2xl items-center justify-between px-4 sm:px-6 lg:px-8">
         <div className="flex items-center space-x-2">
           <Link href="/" className="flex items-center space-x-2">
